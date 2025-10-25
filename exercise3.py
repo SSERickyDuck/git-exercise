5cd8f7c3996752fa63bd0acd887e2944370510f8
Isaak Lux <st200343@stud.uni-stuttgart.de>
2025-10-25 16:54:07 +0200

    combined task a and task b regarding interval check with tolerance

--- exercise3.py
+++ exercise3.py
@@ -32,12 +32,8 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return self.is_in_interval(ll_px - tolerance, 0, self._dx + tolerance) \
-            and self.is_in_interval(ll_py - tolerance, 0, self._dy + tolerance)
-        #   ll_px >= 0 and ll_px <= self._dx \
-        #   and ll_py >= 0 and ll_py <= self._dy
-        #return ll_px >= -tolerance and ll_px <= self._dx + tolerance \
-        #   and ll_py >= -tolerance and ll_py <= self._dy + tolerance
+        return self.is_in_interval(ll_px, 0, self._dx, tolerance) \
+            and self.is_in_interval(ll_py, 0, self._dy, tolerance)
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -45,8 +41,8 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    def is_in_interval(self, i: float, lower: float, upper: float) -> bool: # Task A
-        return lower <= i <= upper
+    def is_in_interval(self, i: float, lower: float, upper: float, tol: float) -> bool: # Task A
+        return (lower - tol) <= i <= (upper + tol)
 
 def test_rectangle_contains_exact() -> None:
     rectangle = Rectangle(lower_left=Point2D(1.0, 2.0), dx=2.5, dy=1.5)
